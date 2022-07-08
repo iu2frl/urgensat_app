cd5ed534d6494151d55f07cd364e9bda530c5c26
Luca <61653175+iu2frl@users.noreply.github.com>
2022-07-08 13:06:30 +0200

    Testing on windows, added bat file

--- top_block.py
+++ top_block.py
@@ -67,7 +67,7 @@ class top_block(gr.top_block, Qt.QWidget):
         ##################################################
         # Blocks
         ##################################################
-        self.qtgui_freq_sink_x_0_0 = qtgui.freq_sink_c(
+        self.qtgui_freq_sink_x_0 = qtgui.freq_sink_c(
         	4096, #size
         	firdes.WIN_BLACKMAN_hARRIS, #wintype
         	0, #fc
@@ -75,21 +75,21 @@ class top_block(gr.top_block, Qt.QWidget):
         	"", #name
         	1 #number of inputs
         )
-        self.qtgui_freq_sink_x_0_0.set_update_time(0.10)
-        self.qtgui_freq_sink_x_0_0.set_y_axis(-140, 10)
-        self.qtgui_freq_sink_x_0_0.set_y_label('Relative Gain', 'dB')
-        self.qtgui_freq_sink_x_0_0.set_trigger_mode(qtgui.TRIG_MODE_FREE, 0.0, 0, "")
-        self.qtgui_freq_sink_x_0_0.enable_autoscale(False)
-        self.qtgui_freq_sink_x_0_0.enable_grid(False)
-        self.qtgui_freq_sink_x_0_0.set_fft_average(1.0)
-        self.qtgui_freq_sink_x_0_0.enable_axis_labels(True)
-        self.qtgui_freq_sink_x_0_0.enable_control_panel(False)
+        self.qtgui_freq_sink_x_0.set_update_time(0.10)
+        self.qtgui_freq_sink_x_0.set_y_axis(-140, 10)
+        self.qtgui_freq_sink_x_0.set_y_label('Relative Gain', 'dB')
+        self.qtgui_freq_sink_x_0.set_trigger_mode(qtgui.TRIG_MODE_FREE, 0.0, 0, "")
+        self.qtgui_freq_sink_x_0.enable_autoscale(False)
+        self.qtgui_freq_sink_x_0.enable_grid(False)
+        self.qtgui_freq_sink_x_0.set_fft_average(1.0)
+        self.qtgui_freq_sink_x_0.enable_axis_labels(True)
+        self.qtgui_freq_sink_x_0.enable_control_panel(False)
 
         if not True:
-          self.qtgui_freq_sink_x_0_0.disable_legend()
+          self.qtgui_freq_sink_x_0.disable_legend()
 
         if "complex" == "float" or "complex" == "msg_float":
-          self.qtgui_freq_sink_x_0_0.set_plot_pos_half(not True)
+          self.qtgui_freq_sink_x_0.set_plot_pos_half(not True)
 
         labels = ['', '', '', '', '',
                   '', '', '', '', '']
@@ -101,22 +101,22 @@ class top_block(gr.top_block, Qt.QWidget):
                   1.0, 1.0, 1.0, 1.0, 1.0]
         for i in xrange(1):
             if len(labels[i]) == 0:
-                self.qtgui_freq_sink_x_0_0.set_line_label(i, "Data {0}".format(i))
+                self.qtgui_freq_sink_x_0.set_line_label(i, "Data {0}".format(i))
             else:
-                self.qtgui_freq_sink_x_0_0.set_line_label(i, labels[i])
-            self.qtgui_freq_sink_x_0_0.set_line_width(i, widths[i])
-            self.qtgui_freq_sink_x_0_0.set_line_color(i, colors[i])
-            self.qtgui_freq_sink_x_0_0.set_line_alpha(i, alphas[i])
+                self.qtgui_freq_sink_x_0.set_line_label(i, labels[i])
+            self.qtgui_freq_sink_x_0.set_line_width(i, widths[i])
+            self.qtgui_freq_sink_x_0.set_line_color(i, colors[i])
+            self.qtgui_freq_sink_x_0.set_line_alpha(i, alphas[i])
 
-        self._qtgui_freq_sink_x_0_0_win = sip.wrapinstance(self.qtgui_freq_sink_x_0_0.pyqwidget(), Qt.QWidget)
-        self.top_grid_layout.addWidget(self._qtgui_freq_sink_x_0_0_win)
-        self.digital_gmsk_mod_0_0 = digital.gmsk_mod(
+        self._qtgui_freq_sink_x_0_win = sip.wrapinstance(self.qtgui_freq_sink_x_0.pyqwidget(), Qt.QWidget)
+        self.top_grid_layout.addWidget(self._qtgui_freq_sink_x_0_win)
+        self.digital_gmsk_mod_0 = digital.gmsk_mod(
         	samples_per_symbol=2,
         	bt=0.35,
         	verbose=False,
         	log=False,
         )
-        self.digital_gmsk_demod_0_0 = digital.gmsk_demod(
+        self.digital_gmsk_demod_0 = digital.gmsk_demod(
         	samples_per_symbol=2,
         	gain_mu=0.175,
         	mu=0.5,
@@ -125,7 +125,7 @@ class top_block(gr.top_block, Qt.QWidget):
         	verbose=False,
         	log=False,
         )
-        self.channels_channel_model_0_0 = channels.channel_model(
+        self.channels_channel_model_0 = channels.channel_model(
         	noise_voltage=50,
         	frequency_offset=0.0,
         	epsilon=1.0,
@@ -133,16 +133,16 @@ class top_block(gr.top_block, Qt.QWidget):
         	noise_seed=0,
         	block_tags=False
         )
-        self.blocks_throttle_0_0 = blocks.throttle(gr.sizeof_char*1, samp_rate,True)
-        self.blocks_tcp_server_sink_0 = blocks.tcp_server_sink(gr.sizeof_char*1, '', 1239, True)
-        self.blocks_multiply_const_vxx_1_0 = blocks.multiply_const_vcc((500, ))
-        self.blks2_tcp_source_0_0 = grc_blks2.tcp_source(
+        self.blocks_throttle_0 = blocks.throttle(gr.sizeof_char*1, samp_rate,True)
+        self.blocks_tcp_server_sink_1 = blocks.tcp_server_sink(gr.sizeof_char*1, '', 1235, True)
+        self.blocks_multiply_const_vxx_1 = blocks.multiply_const_vcc((500, ))
+        self.blks2_tcp_source_0_0_0 = grc_blks2.tcp_source(
         	itemsize=gr.sizeof_char*1,
         	addr='127.0.0.1',
-        	port=5002,
+        	port=5001,
         	server=True,
         )
-        self.blks2_packet_encoder_0_0 = grc_blks2.packet_mod_b(grc_blks2.packet_encoder(
+        self.blks2_packet_encoder_0 = grc_blks2.packet_mod_b(grc_blks2.packet_encoder(
         		samples_per_symbol=1,
         		bits_per_symbol=1,
         		preamble='',
@@ -151,10 +151,10 @@ class top_block(gr.top_block, Qt.QWidget):
         	),
         	payload_length=1,
         )
-        self.blks2_packet_decoder_0_0 = grc_blks2.packet_demod_b(grc_blks2.packet_decoder(
+        self.blks2_packet_decoder_0 = grc_blks2.packet_demod_b(grc_blks2.packet_decoder(
         		access_code='',
         		threshold=-1,
-        		callback=lambda ok, payload: self.blks2_packet_decoder_0_0.recv_pkt(ok, payload),
+        		callback=lambda ok, payload: self.blks2_packet_decoder_0.recv_pkt(ok, payload),
         	),
         )
 
@@ -163,15 +163,15 @@ class top_block(gr.top_block, Qt.QWidget):
         ##################################################
         # Connections
         ##################################################
-        self.connect((self.blks2_packet_decoder_0_0, 0), (self.blocks_tcp_server_sink_0, 0))
-        self.connect((self.blks2_packet_encoder_0_0, 0), (self.digital_gmsk_mod_0_0, 0))
-        self.connect((self.blks2_tcp_source_0_0, 0), (self.blocks_throttle_0_0, 0))
-        self.connect((self.blocks_multiply_const_vxx_1_0, 0), (self.channels_channel_model_0_0, 0))
-        self.connect((self.blocks_throttle_0_0, 0), (self.blks2_packet_encoder_0_0, 0))
-        self.connect((self.channels_channel_model_0_0, 0), (self.digital_gmsk_demod_0_0, 0))
-        self.connect((self.channels_channel_model_0_0, 0), (self.qtgui_freq_sink_x_0_0, 0))
-        self.connect((self.digital_gmsk_demod_0_0, 0), (self.blks2_packet_decoder_0_0, 0))
-        self.connect((self.digital_gmsk_mod_0_0, 0), (self.blocks_multiply_const_vxx_1_0, 0))
+        self.connect((self.blks2_packet_decoder_0, 0), (self.blocks_tcp_server_sink_1, 0))
+        self.connect((self.blks2_packet_encoder_0, 0), (self.digital_gmsk_mod_0, 0))
+        self.connect((self.blks2_tcp_source_0_0_0, 0), (self.blocks_throttle_0, 0))
+        self.connect((self.blocks_multiply_const_vxx_1, 0), (self.channels_channel_model_0, 0))
+        self.connect((self.blocks_throttle_0, 0), (self.blks2_packet_encoder_0, 0))
+        self.connect((self.channels_channel_model_0, 0), (self.digital_gmsk_demod_0, 0))
+        self.connect((self.channels_channel_model_0, 0), (self.qtgui_freq_sink_x_0, 0))
+        self.connect((self.digital_gmsk_demod_0, 0), (self.blks2_packet_decoder_0, 0))
+        self.connect((self.digital_gmsk_mod_0, 0), (self.blocks_multiply_const_vxx_1, 0))
 
     def closeEvent(self, event):
         self.settings = Qt.QSettings("GNU Radio", "top_block")
@@ -183,8 +183,8 @@ class top_block(gr.top_block, Qt.QWidget):
 
     def set_samp_rate(self, samp_rate):
         self.samp_rate = samp_rate
-        self.qtgui_freq_sink_x_0_0.set_frequency_range(0, self.samp_rate)
-        self.blocks_throttle_0_0.set_sample_rate(self.samp_rate)
+        self.qtgui_freq_sink_x_0.set_frequency_range(0, self.samp_rate)
+        self.blocks_throttle_0.set_sample_rate(self.samp_rate)
 
 
 def main(top_block_cls=top_block, options=None):
