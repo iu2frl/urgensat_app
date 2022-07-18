007d077bfb688a0fc11bca5beba606b12da97df9
Luca Bennati <102149845+asemlucben@users.noreply.github.com>
2022-07-18 16:05:43 +0200

    Project cleanup

--- urgensat/original/app.py
+++
@@ -1,7 +0,0 @@
-import helpers
-
-command_handler,station,config = helpers.setup()
-
-while(True):
-    message = input()
-    command_handler.parse_command(message)
--- urgensat/original/handler/iot_handler.py
+++
@@ -1,3 +0,0 @@
-class IOTHandler:
-    def __init__(self,device_list):
-        self.device_list = device_list
